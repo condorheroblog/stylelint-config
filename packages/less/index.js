d47dc8e8d8705f44494d496082e03176dbe8553d
CondorHero <love2xinwei@gmail.com>
2022-06-13 16:19:51 +0800

    ci: add stylelint-config-recommended-less

--- packages/less/index.js
+++ packages/less/index.js
@@ -1,7 +1,7 @@
 const stylelintCSS = require("@condorhero/stylelint-config-css");
 
 module.exports = {
-  extends: ["@condorhero/stylelint-config-css"],
+  extends: ["@condorhero/stylelint-config-css", "stylelint-config-recommended-less"],
   overrides: [
     {
       files: ["**/*.less"],
